72cb03a187516a84d24f1d82f0db22907309cb66
DavidBistron <154263696+DavidBistron@users.noreply.github.com>
2024-10-29 12:45:43 +0100

    Add css and js

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeColor() {
+    if (document.getElementById("colorButton").style.color === "black") {
+        document.getElementById("colorButton").style.color = "red";
+    } else {
+        document.getElementById("colorButton").style.color = "black";
+    }
+}
